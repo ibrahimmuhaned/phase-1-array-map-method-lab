60bcba2038fb5badeb46817159e0b360f20c91c2
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-03-30 22:34:44 +0300

    done

--- index.js
+++ index.js
@@ -12,5 +12,21 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    // Split the title into words
+    const words = title.split(' ');
+
+    // Capitalize the first letter of each word
+    const capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+
+    // Join the words back into a title
+    return capitalizedWords.join(' ');
+  });
 }
+
+module.exports = {
+  tutorials,
+  titleCased
+};
